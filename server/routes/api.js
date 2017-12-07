e601503a6053f508bcb52088a37d82bfce2d2208
azhariemuhammad <azharie_muhammad@live.com>
2017-12-07 14:06:46 +0700

    crud user done

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,6 +1,19 @@
 const express = require('express')
 const router = express.Router()
 const photoController = require('../controllers/photoCont')
+const userController = require('../controllers/user-Cont')
+
+
+router.post('/users', userController.createUser)
+
+router.get('/users/:email', userController.findById)
+
+router.put('/users/:id', userController.findByIdAndUpdate)
+
+router.get('/users', userController.getAllUsers)
+
+router.delete('/users/:id', userController.findByIdAndRemove)
+
 
 router.post('/photos', photoController.create)
 
